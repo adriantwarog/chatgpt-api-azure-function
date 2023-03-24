57f2f7bf9b912272216bbf617c366de6aa69f9a1
Adrian Twarog <adrian@moshimoshi.marketing>
2023-03-24 13:06:30 +0800

    Initial Commit

---
+++ src/functions/gptfunction.js
@@ -0,0 +1,29 @@
+const { app } = require('@azure/functions');
+const { Configuration, OpenAIApi } =require("openai");
+const configuration = new Configuration({
+	organization: "org-i16GdI3biVetsydNnhzCYRLs",
+	apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.http('gptfunction', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        
+        const { messages } = await request.json();
+
+        context.log(messages)
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {"role": "system", "content": "You are DesignGPT helpful assistant graphics design chatbot"},
+                ...messages
+            ]
+        })
+    
+        return { jsonBody: {
+            completion: completion.data.choices[0].message
+        }};
+    }
+});
